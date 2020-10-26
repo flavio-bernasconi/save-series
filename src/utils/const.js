24e6d4fbc552b4cc8b78cfd7c93e4b187cde5b2c
flavio-bernasconi <info@flaviobernasconi.it>
2020-10-26 14:35:49 +0100

    minor refactor

--- src/utils/const.js
+++ src/utils/const.js
@@ -3,9 +3,9 @@ export const initialNumberOfPages = 1;
 export const transition = { ease: [0.6, 0.01, -0.05, 0.9] };
 
 export const DB_FOLDERS = [
-  { name: "to-watch", label: "to watch", color: "#28B5A9" },
-  { name: "watching", label: "watching", color: "#6566AB" },
-  { name: "watched", label: "watched", color: "white" },
+  { name: "to-watch", label: "to watch", color: "#666" },
+  { name: "watching", label: "watching", color: "#333" },
+  { name: "watched", label: "watched", color: "#111" },
 ];
 
 // 007EA7
